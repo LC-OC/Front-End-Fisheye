ac8f6af88c393307545af4f27548c608133a11bf
Louise Chartier <louise-chartier@outlook.fr>
2022-03-24 14:08:25 +0100

    add data factory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,11 +7,25 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.classList.add("photographe_picture");
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h2.classList.add("photographe_name");
+        const pCity = document.createElement('p');
+        pCity.textContent = city;
+        pCity.classList.add("photographe_city");
+        const pTagline = document.createElement('p');
+        pTagline.textContent = tagline;
+        pTagline.classList.add("photographe_tagline");
+        const pPrice = document.createElement('p');
+        pPrice.textContent = price + '€/jour';
+        pPrice.classList.add("photographe_price");
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pCity);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
         return (article); 
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, tagline, price, getUserCardDOM }
 }
