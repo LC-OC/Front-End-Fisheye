10af75a4702404b1d0cf8d0af982666924c8d2a9
Louise Chartier <louise-chartier@outlook.fr>
2022-03-24 13:20:15 +0100

    fetch data photographers

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -11,7 +11,7 @@ function photographerFactory(data) {
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        return (article); 
     }
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,21 @@
-    async function getPhotographers() {
+   /*async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
+
+        // Récupération des données avec Fetch (apparaissent bien dans la console)
+        let myRequest = new Request("./data/photographers.json");
+        fetch(myRequest)
+            .then(function(resp) {
+                return resp.json();
+            })
+            .then(function(data) {
+                console.log(data.photographers);
+                let photographers = data.photographers;
+                return photographers;
+            })
+
+        
+            
+        /*const photographers = [            
             {
                 "name": "Ma data test",
                 "id": 1,
@@ -19,11 +34,14 @@
                 "price": 500,
                 "portrait": "account.png"
             },
+            
+            
         ]
         // et bien retourner le tableau photographers seulement une fois
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
+    
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -34,12 +52,29 @@
             photographersSection.appendChild(userCardDOM);
         });
     };
-
     async function init() {
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
+        
     };
     
-    init();
-    
+    init(); */
+
+    // Test Récupération données
+
+    const photographersSection = document.querySelector(".photographer_section");
+
+    let myRequest = new Request("./data/photographers.json");
+    fetch(myRequest)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.photographers)
+            for (const photographer of data.photographers) {
+                const photographerModel = photographerFactory(photographer);
+                const userCardDOM = photographerModel.getUserCardDOM();
+                photographersSection.appendChild(userCardDOM);
+
+            }
+        })
+        .catch(console.error);
